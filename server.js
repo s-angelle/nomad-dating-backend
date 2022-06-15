bcd60076c29fa3f48257e5efc5a790193ab67bc4
Sabrina A <sabrina@Sabrinas-MacBook-Pro.local>
2022-06-15 09:40:20 -0500

    created index route and controller for movies / connected to db

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-// IMPORT
+// Import
 const express = require('express');
 const app = express();
 require('dotenv').config();
+require('./config/database');
 
-// ROUTES
+// === Routes ===
+
+// Movies
+app.use('/api/v1/movies', require('./routes/api/movies.js'));
 
 // PORT
 const PORT = 8080;
