457fcfc089fd9aeeb73eebefcf93a25c9cbe8e37
Sabrina A <sabrina@Sabrinas-MBP.net>
2022-06-29 19:54:51 -0500

    organized code

--- models/Profile.js
+++ models/Profile.js
@@ -1,28 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const profileSchema = new Schema({
+const profileSchema = new Schema(
+  {
     name: {
-       type: String,
-       required: true 
+      type: String,
+      required: true,
     },
     age: {
-        type: Number,
-        required: true 
-     },
-    about: {
-        type: String,
-        required: true 
-     },
-    image:{
-        type: String,
-        required: true 
-     },
-    interests: String,
+      type: Number,
+      required: true,
+    },
     job: String,
     school: String,
-}, {
-    timestamps: true
-});
+    image: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+    },
+    interests: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Profile', profileSchema);
+module.exports = mongoose.model("Profile", profileSchema);
